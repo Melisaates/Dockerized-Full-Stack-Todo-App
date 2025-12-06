03e2e60094812df3ef2fc3ea8cd55fbf27d8cd75
Melisaates <101855839+Melisaates@users.noreply.github.com>
2025-12-06 11:51:54 +0300

    feat: add PostgreSQL support with Prisma adapter and update schema configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,17 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  
-  constructor() {
+    constructor() {
+    const accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
+    
     super({
+      ...(accelerateUrl && { accelerateUrl }),
       log: ['query', 'info', 'warn', 'error'],
     });
+    
+    console.log('PRISMA_ACCELERATE_URL:', accelerateUrl ? 'Set' : 'Not set');
   }
-
+  
   async onModuleInit() {
     await this.$connect();
   }
